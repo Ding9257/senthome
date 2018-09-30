15ef23a2f328682749b4e5bf9f8336846592621c
丁加宁 <dingjn@i-counting.cn>
2018-09-30 14:30:20 +0800

    初始化项目

---
+++ pages/index/index.js
@@ -0,0 +1,34 @@
+var util = require('../../utils/util.js');
+var api = require('../../utils/util.js');
+
+Page({
+    data: {
+        banner: [
+            {
+                id: 1,
+                link: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwin7Lu47t_dAhWMy7wKHQiWAT8QjRx6BAgBEAU&url=https%3A%2F%2Fmedium.com%2F%40sidneyliebrand%2Fswitching-to-asdf-version-manager-eb6569e4e562&psig=AOvVaw2x5Z-EUT_Zx8soKrbz27JY&ust=1538298451097129",
+                image_url: "https://images7.alphacoders.com/303/thumb-1920-303042.png"
+            },
+            {
+                id: 2,
+                link: "https://images3.alphacoders.com/823/823520.jpg",
+                image_url: "https://images3.alphacoders.com/823/823520.jpg"
+            }
+        ]
+    },
+    onLoad: function (options) {
+        //this.getCatalog();
+    },
+    onReady: function () {
+        // 页面渲染完成
+    },
+    onShow: function () {
+        // 页面显示
+    },
+    onHide: function () {
+        // 页面隐藏
+    },
+    onUnload: function () {
+        // 页面关闭
+    }
+})
