fd24e25e937ade7b5916f2d7379dfef8e49b57f0
丁加宁 <dingjn@i-counting.cn>
2018-10-17 19:01:02 +0800

    我的订单

--- home/view/order/index.js
+++ home/view/order/index.js
@@ -11,49 +11,20 @@ Page({
         is_load: false,
         list: [],
         order_flow: '',
-        order_list: []
+        order_list: [
+            {id:1},
+            {id:1},
+            {id:1},
+            {id:1},{id:1},
+            {id:1},
+            {id:1}
+        ]
     },
     onUnload: function () {
 
     },
     onLoad: function (option) {
-        http.request({
-            url: '/order/list',
-            data: {
-                page_index: 0,
-                page_size: 0
-            },
-            success: function (data) {
-                var order_list = [];
 
-                for (var i = 0; i < data.length; i++) {
-                    if (data[i].order_flow == option.order_flow || option.order_flow == 'ALL') {
-                        order_list.push(data[i]);
-                    }
-                }
-
-                var index = 0;
-                for (var i = 0; i < this.data.order_status_list.length; i++) {
-                    if (option.order_flow == this.data.order_status_list[i].order_status_value) {
-                        index = i;
-
-                        break;
-                    }
-                }
-
-                var slider_width = this.data.window_width / this.data.order_status_list.length;
-
-                this.setData({
-                    slider_left: 0,
-                    slider_offset: slider_width * index,
-                    slider_width: slider_width,
-                    is_load: true,
-                    list: data,
-                    order_flow: option.order_flow,
-                    order_list: order_list
-                });
-            }.bind(this)
-        });
     },
     onReady: function () {
 
